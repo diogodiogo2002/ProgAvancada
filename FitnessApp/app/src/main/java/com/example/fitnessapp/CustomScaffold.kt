b4a8eb098875c418a3077940541d9f4454d06456
diogodiogo2002 <diogojmd17@gmail.com>
2024-06-13 17:21:53 +0100

    Adicionado botão de back e class CustomScaffold
    CustomScaffold classe que controla o conteudo de cada screen e renderiza o botao de back

---
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/CustomScaffold.kt
@@ -0,0 +1,36 @@
+package com.example.fitnessapp
+
+import android.annotation.SuppressLint
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomScaffold(navController: NavController, showBackButton: Boolean, content: @Composable () -> Unit) {
+    Scaffold(
+        topBar = {
+            if (showBackButton) {
+
+                TopAppBar(
+                    title = { Text("Back") },
+                    navigationIcon = {
+                        IconButton(onClick = { navController.popBackStack() }) {
+                            Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                        }
+                    }
+                )
+            }
+        }
+    ) {
+        content()
+    }
+}
--- FitnessApp/app/src/main/java/com/example/fitnessapp/GainWeightScreen.kt
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/GainWeightScreen.kt
@@ -4,21 +4,35 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GainWeightScreen(navController: NavController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Você está na tela de ganhar peso!", modifier = Modifier.padding(16.dp))
+    CustomScaffold(navController = navController, showBackButton = true) {
+        Column {
+            TopAppBar(
+                title = { Text("Gain Weight Screen") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                    }
+                },
+
+            )
+            Text("Content of Gain Weight Screen")
+        }
     }
 }
 
