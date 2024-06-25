096698719717f4f28521626cfa44751035f24e46
diogodiogo2002 <diogojmd17@gmail.com>
2024-06-25 20:58:13 +0100

    Adicionado Logo Menu Screen

--- FitnessApp/app/src/main/java/com/example/fitnessapp/MenuScreen.kt
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/MenuScreen.kt
@@ -1,14 +1,16 @@
 package com.example.fitnessapp
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-
+import com.example.fitnessapp.R.drawable
 @Composable
 fun MenuScreen(navController: NavController) {
     Column(
@@ -18,6 +20,15 @@ fun MenuScreen(navController: NavController) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+
+        Image(
+            painter = painterResource(id = drawable.banner),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(bottom = 32.dp)
+        )
         Text(text = "O que deseja fazer?", modifier = Modifier.padding(bottom = 32.dp))
 
         Button(onClick = { navController.navigate("lose_weight") }) {
