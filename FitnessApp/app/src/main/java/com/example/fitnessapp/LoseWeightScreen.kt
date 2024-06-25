61a5a2cd13ce75028422b3fb7eb9762dd71b2408
diogodiogo2002 <diogojmd17@gmail.com>
2024-06-25 21:04:13 +0100

    Adicionada Imagem LoseWeightScreen

--- FitnessApp/app/src/main/java/com/example/fitnessapp/LoseWeightScreen.kt
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/LoseWeightScreen.kt
@@ -1,9 +1,11 @@
 package com.example.fitnessapp
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -16,6 +18,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,6 +38,14 @@ fun LoseWeightScreen(navController: NavController) {
                 },
 
                 )
+            Image(
+                painter = painterResource(id = R.drawable.loseweight),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .padding(bottom = 32.dp)
+            )
             Text("TIPS FOR LOSING WEIGHT",
                 fontSize = 32.sp, // Using the custom fontSize modifier
                 fontWeight = FontWeight.Bold
